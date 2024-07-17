7d95fc137600c169951afade2a88af089ecd4d51
Yassine Idrissi <yaidriss@c1r4p7.1337.ma>
2024-07-17 22:31:23 +0100

    update cmd commands file

--- commands/cmd.cpp
+++ commands/cmd.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cmd.cpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rchahban <rchahban@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: yaidriss <yaidriss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 23:47:44 by yaidriss          #+#    #+#             */
-/*   Updated: 2024/07/16 16:57:08 by rchahban         ###   ########.fr       */
+/*   Updated: 2024/07/17 22:29:51 by yaidriss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,15 @@ void Server::cmd_parser(std::string &msg, int fd)
 {
 	
 	std::cout << "Client " << fd << " sent: " << msg << std::endl;
-	if (!msg.compare(0, 4, "BONG") || !msg.compare(0, 4, "bong"))
+	if (!msg.compare(0, 4, "PASS") || !msg.compare(0, 4, "pass"))
+		this->pass(msg, fd);
+	else if(this->getClient(fd)->getIsLoggedIn() == false)
+		senderreur(fd, "451 :You have not registered");
+	else if (!msg.compare(0, 4, "BONG") || !msg.compare(0, 4, "bong"))
 		return;
 	else if (!msg.compare(0, 4, "PING") || !msg.compare(0, 4, "ping"))
 		// this->ping(fd, cmd);
 		std::cout << "PING :" << msg << std::endl;
-	else if (!msg.compare(0, 4, "PASS") || !msg.compare(0, 4, "pass"))
-		this->pass(msg, fd);
 		// std::cout << "PASS :" << msg << std::endl;
 	else if (!msg.compare(0, 4, "USER") || !msg.compare(0, 4, "user"))
 		this->user(msg, fd);
@@ -55,5 +57,5 @@ void Server::cmd_parser(std::string &msg, int fd)
 		this->invite(msg, fd);
 		// std::cout << "INVITE :" << msg << std::endl;
 	else
-		send(fd, "500 Command not recognized.\n", 28, 0);
+		senderreur(fd, "421 :Unknown command");
 }
