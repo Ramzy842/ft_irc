e3072c15f436ec4e8c97c76330412522201a591c
Yassine Idrissi <yaidriss@c1r7p1.1337.ma>
2024-07-23 04:19:26 +0100

    add quit function

--- commands/cmd.cpp
+++ commands/cmd.cpp
@@ -6,7 +6,7 @@
 /*   By: yaidriss <yaidriss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 23:47:44 by yaidriss          #+#    #+#             */
-/*   Updated: 2024/07/21 16:59:14 by yaidriss         ###   ########.fr       */
+/*   Updated: 2024/07/23 03:53:27 by yaidriss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,108 +14,26 @@
 
 void Server::handler_cmd_parser(std::string &msg, int fd)
 {
-	// std::cout << "Client " << fd << " sent: " << msg << std::endl;
-	// std::vector<std::string> cmd = split_command(msg);
-	// if(getClient(fd)->getIsHexChat() || cmd[0] == "CAP")
-	// {
-	// 	if (cmd[0] == "CAP" && cmd.size() < 3)
-	// 		getClient(fd)->setIsHexChat(true);
-	// 	else if(cmd[0] == "CAP" && cmd.size() < 5)
-	// 	{
-	// 		std::cout << "im here  2" << std::endl;
-	// 		std::string msg = cmd[2] + " " + cmd[3];
-	// 		this->pass(msg, fd);
-	// 		std::cout << "111111111 hna PASS :" << msg << std::endl;	
-	// 	}
-	// 	else if(cmd[0] == "CAP" && cmd.size() > 4)
-	// 	{
-	// 		std::cout << "im here"	<< std::endl;
-	// 		std::string msg = cmd[2] + " " + cmd[3];
-	// 		this->pass(msg, fd);
-	// 		std::cout << "111111111 hna PASS :" << msg << std::endl;
-	// 		msg = cmd[4] + " " + cmd[5];
-	// 		std::cout << "NICK :" << msg << std::endl;
-	// 		this->nick(msg, fd);
-	// 		std::cout << "nick :" << msg << std::endl;
-	// 		msg = cmd[6] + " " + cmd[7] + " " + cmd[7] + " " + cmd[7] + " " + cmd[9].substr(1);
-	// 		std::cout << "USER :" << msg << "*/*/&&&&&&&&&&" << std::endl;
-	// 		this->user(msg, fd);
-			
-	// 	}
-	// 	else if (!msg.compare(0, 4, "BONG") || !msg.compare(0, 4, "bong"))
-	// 		return;
-	// 	else if (!msg.compare(0, 4, "PING") || !msg.compare(0, 4, "ping"))
-	// 		std::cout << "PING :" << msg << std::endl;
-	// 	else if (!msg.compare(0, 4, "JOIN") || !msg.compare(0, 4, "join"))
-	// 		this->join(msg, fd);
-	// 		// std::cout << "JOIN :" << msg << std::endl;
-	// 	else if (!msg.compare(0, 4, "PART") || !msg.compare(0, 4, "part"))
-	// 		// this->part(msg, fd);
-	// 		std::cout << "PART :" << msg << std::endl;
-	// 	else if (!msg.compare(0, 4, "KICK") || !msg.compare(0, 4, "kick"))
-	// 		this->kick(msg, fd);
-	// 		// std::cout << "KICK :" << msg << std::endl;
-	// 	else if (!msg.compare(0, 7, "PRIVMSG") || !msg.compare(0, 7, "privmsg"))
-	// 		this->privmsg(msg, fd);
-	// 		// std::cout << "PRIVMSG :" << msg << std::endl;
-	// 	else if (!msg.compare(0, 5, "TOPIC") || !msg.compare(0, 5, "topic"))
-	// 		this->topic(msg, fd);
-	// 		// std::cout << "TOPIC :" << msg << std::endl;
-	// 	else if (!msg.compare(0, 4, "MODE") || !msg.compare(0, 4, "mode"))
-	// 		this->mode(msg, fd);
-	// 		// std::cout << "MODE :" << msg << std::endl;
-	// 	else if (!msg.compare(0, 4, "QUIT") || !msg.compare(0, 4, "quit"))
-	// 		// this->quit(msg, fd);
-	// 		std::cout << "QUIT :" << msg << std::endl;
-	// 	else if (!msg.compare(0, 6, "INVITE") || !msg.compare(0, 6, "invite"))
-	// 		this->invite(msg, fd);
-	// 		// std::cout << "INVITE :" << msg << std::endl;
-	// 	else
-	// 		senderreur(fd, "421 :Unknown command");
-		
-	// }
-	// else
-	// {
 		if (!msg.compare(0, 4, "PASS") || !msg.compare(0, 4, "pass"))
 			this->pass(msg, fd);
-		// else if(!this->getClient(fd)->getIsLoggedIn())
-		// 	senderreur(fd, "451 :You have not logged in");
 		else if (!msg.compare(0, 4, "NICK") || !msg.compare(0, 4, "nick"))
 			this->nick(msg, fd);
-		// else if(!this->getClient(fd)->getIsNickSet())
-		// 	senderreur(fd, "451 :You have not set your nickname");
 		else if (!msg.compare(0, 4, "USER") || !msg.compare(0, 4, "user"))
 			this->user(msg, fd);
-		// else if(!this->getClient(fd)->getIsRegistered())
-		// 	senderreur(fd, "451 :You have not registered");		
-		else if (!msg.compare(0, 4, "BONG") || !msg.compare(0, 4, "bong"))
-			return;
-		else if (!msg.compare(0, 4, "PING") || !msg.compare(0, 4, "ping"))
-			std::cout << "PING :" << msg << std::endl;
 		else if (!msg.compare(0, 4, "JOIN") || !msg.compare(0, 4, "join"))
 			this->join(msg, fd);
-			// std::cout << "JOIN :" << msg << std::endl;
-		else if (!msg.compare(0, 4, "PART") || !msg.compare(0, 4, "part"))
-			// this->part(msg, fd);
-			std::cout << "PART :" << msg << std::endl;
 		else if (!msg.compare(0, 4, "KICK") || !msg.compare(0, 4, "kick"))
 			this->kick(msg, fd);
-			// std::cout << "KICK :" << msg << std::endl;
 		else if (!msg.compare(0, 7, "PRIVMSG") || !msg.compare(0, 7, "privmsg"))
 			this->privmsg(msg, fd);
-			// std::cout << "PRIVMSG :" << msg << std::endl;
 		else if (!msg.compare(0, 5, "TOPIC") || !msg.compare(0, 5, "topic"))
 			this->topic(msg, fd);
-			// std::cout << "TOPIC :" << msg << std::endl;
 		else if (!msg.compare(0, 4, "MODE") || !msg.compare(0, 4, "mode"))
 			this->mode(msg, fd);
-			// std::cout << "MODE :" << msg << std::endl;
 		else if (!msg.compare(0, 4, "QUIT") || !msg.compare(0, 4, "quit"))
-			// this->quit(msg, fd);
-			std::cout << "QUIT :" << msg << std::endl;
+			this->quit(msg, fd);
 		else if (!msg.compare(0, 6, "INVITE") || !msg.compare(0, 6, "invite"))
 			this->invite(msg, fd);
-			// std::cout << "INVITE :" << msg << std::endl;
 		else
 			senderreur(fd, "421 :Unknown command");
 }
--- commands/quit.cpp
+++ commands/quit.cpp
@@ -1,22 +1,34 @@
 #include "cmd.hpp"
 
-// void Server::quit(std::string &msg, int fd)
-// {
-// 	std::vector<std::string> cmd = split_command(msg);
-// 	Client *client = this->getClient(fd);
-// 	if (client->getIsRegistered())
-// 	{
-// 		for(std::vector<Channel>::iterator it = this->channels.begin(); it != this->channels.end(); ++it)
-// 		{
-// 			if (it->getMemberByName(client->getNickname()))
-// 			{
-// 				it->removeMember(*client);
-// 				if(cmd.size() > 1)
-// 					it->sendToAll(client->getNickname() + " has quit (" + cmd[1] + ")");
-// 				else
-// 					it->sendToAll(client->getNickname() + " has quit");
-// 			}
-// 		}
-// 	}
-// 	close(fd);
-// }
+#define ERR_NEEDMOREPARAMS(command) "461 " + command + " :Not enough parameters"
+
+void Server::quit(std::string &msg, int fd)
+{
+	std::vector<std::string> cmd = split_command(msg);
+	if(cmd.size() > 2)
+	{
+		senderreur(fd, ERR_NEEDMOREPARAMS(cmd[0]));
+		return;
+	}
+	Client *client = this->getClient(fd);
+	if (client->getIsRegistered())
+	{
+		for(std::vector<Channel *>::iterator it = this->channels.begin(); it != this->channels.end(); ++it)
+		{
+			if ((*it)->getMemberByName(client->getNickname()))
+			{
+				(*it)->removeMember(*client);
+				if ((*it)->getOperatorByName(client->getNickname()))
+					(*it)->removeOperator(*client);
+				std::string msg;
+				if(cmd.size() == 2)
+					msg = ":" + client->getNickname() + "!~" + client->getHostname() + "@localhost QUIT " + cmd[1];
+				else
+					msg = ":" + client->getNickname() + "!~" + client->getHostname() + "@localhost QUIT";
+				for(size_t i = 0; i < (*it)->getMembers().size(); i++)
+					sendMsg((*it)->getMembers()[i]->getFd(), msg);
+			}
+		}
+	}
+	close(fd);
+}
