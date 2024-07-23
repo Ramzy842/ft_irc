d1e2f66c4f9c22e110e1eb14b6984a503a4ddc06
Yassine Idrissi <yaidriss@c1r7p1.1337.ma>
2024-07-23 04:44:43 +0100

    fix kick command

--- commands/kick.cpp
+++ commands/kick.cpp
@@ -6,7 +6,7 @@
 /*   By: yaidriss <yaidriss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/13 00:14:06 by yaidriss          #+#    #+#             */
-/*   Updated: 2024/07/23 02:26:56 by yaidriss         ###   ########.fr       */
+/*   Updated: 2024/07/23 04:43:45 by yaidriss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,9 @@
 #define ERR_CHANOPRIVSNEEDED(channel) "482 " + channel + " :You're not channel operator"
 #define ERR_NEEDMOREPARAMS(command) "461 " + command + " :Not enough parameters"
 
-// class Client;
 
 Channel *Server::handlerkickcommand(std::vector<std::string> cmd, int fd)
 {
-    // if (cmd.size() < 2 )//|| !isEmpyCmd(cmd))
-    // {
-    //     senderreur(fd, ERR_NEEDMOREPARAMS(cmd[0]));
-    //     return NULL;
-    // }
 	std::cout << "cmd :" << cmd[1] << cmd[2] << std::endl;
     Channel *channel = this->getChannelByName(cmd[1].substr(1));
     if(!channel)
@@ -61,35 +55,12 @@ void Server::kick(std::string &msg, int fd)
     Channel* channel = handlerkickcommand(cmd,fd);
     if(!channel)
         return;
-    channel->removeMember(*channel->getMemberByName(cmd[2]));
     std::string msj = ":" + this->getClient(fd)->getNickname() + "!~" + this->getClient(fd)->getHostname() + "@localhost KICK " + cmd[1] + " " + cmd[2];
     for(size_t i = 0; i < channel->getMembers().size(); i++)
-    {
-        // if(channel->getMembers()[i]->getFd() != fd)
             sendMsg(channel->getMembers()[i]->getFd(), msj);
-    }
     if(channel->getOperatorByName(cmd[2]))
-    {
       channel->removeOperator(*channel->getOperatorByName(cmd[2]));
-    }
-    //channel->removeMember(*channel->getMemberByName(cmd[2])); 
+    channel->removeMember(*channel->getMemberByName(cmd[2]));
     this->getClientByName(cmd[2])->removeChannel(*channel);
     sendMsg(getClientByName(cmd[2])->getFd(), msj);
-
-
-    // if(cmd.size() == 3)
-    //     kickmsg += "\n";
-    // else
-    // {
-    //     kickmsg += " :";
-    //     for(size_t i = 3; i < cmd.size(); i++)
-    //         kickmsg += cmd[i] + " ";
-    //     kickmsg += "\n";
-    // }
-    // for (size_t i = 0; i < channel->getMembers().size(); i++)
-    // {
-    //     if(channel->getMembers()[i]->getFd() != fd)
-    //         sendMsg(channel->getMembers()[i]->getFd(), msg);
-    // }
-    // sendMsg(fd, msj);
 }
